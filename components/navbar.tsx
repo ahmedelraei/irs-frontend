b10ea0df856a023ef38f4c56b8e5075a65b41222
Ahmed Hatem <ahmedelrai18@gmail.com>
2025-02-24 11:11:17 +0200

    fix: navbar menu closes after navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import * as React from "react";
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
@@ -29,14 +29,20 @@ import {
   Logo,
 } from "@/components/icons";
 import LoginModal from "./LoginModal";
+import { useRouter } from 'next/navigation';
+
 
 export const Navbar = () => {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const router = useRouter();
 
   return (
     <>
       <LoginModal isOpen={isOpen} onOpenChange={onOpenChange} />
-      <HeroUINavbar maxWidth="xl" position="sticky">
+      <HeroUINavbar maxWidth="xl" position="sticky" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
         <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
           <NavbarBrand as="li" className="gap-3 max-w-fit">
             <NextLink className="flex justify-start items-center gap-1" href="/">
@@ -50,7 +56,7 @@ export const Navbar = () => {
                 <NextLink
                   className={clsx(
                     linkStyles({ color: "foreground" }),
-                    "data-[active=true]:text-primary data-[active=true]:font-medium",
+                    "data-[active=true]:text-primary data-[active=true]:font-medium"
                   )}
                   color="foreground"
                   href={item.href}
@@ -89,7 +95,6 @@ export const Navbar = () => {
         </NavbarContent>
 
         <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
@@ -105,29 +110,31 @@ export const Navbar = () => {
           <NavbarMenuToggle />
         </NavbarContent>
 
-        <NavbarMenu>
-          <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navMenuItems.map((item, index) => (
-              <NavbarMenuItem key={`${item}-${index}`}>
-                <Link
-                  color={
-                    index === 2
-                      ? "primary"
-                      : index === siteConfig.navMenuItems.length - 1
+        {isMenuOpen && (
+          <NavbarMenu>
+            <div className="mx-4 mt-2 flex flex-col gap-2">
+              {siteConfig.navMenuItems.map((item, index) => (
+                <NavbarMenuItem key={`${item}-${index}`}>
+                  <Link
+                    color={
+                      index === 2
+                        ? "primary"
+                        : index === siteConfig.navMenuItems.length - 1
                         ? "danger"
                         : "foreground"
-                  }
-                  href="#"
-                  size="lg"
-                >
-                  {item.label}
-                </Link>
-              </NavbarMenuItem>
-            ))}
-          </div>
-        </NavbarMenu>
+                    }
+                    size="lg"
+                    href={item.href}
+                    onPress={() => {setIsMenuOpen(false); router.push(item.href)}}
+                  >
+                    {item.label}
+                  </Link>
+                </NavbarMenuItem>
+              ))}
+            </div>
+          </NavbarMenu>
+        )}
       </HeroUINavbar>
     </>
-
   );
 };
